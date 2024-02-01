de481a80282d5efed6d46fbd1cfb16c63d2f34c5
MizaelJR17 <mizael.jxjunior@aluno.educacao.pe.gov.br]>
2024-02-01 20:28:03 -0300

    Entidades de produtos

--- backend/src/Core/Ecommerce.Domain/Product.cs
+++ backend/src/Core/Ecommerce.Domain/Product.cs
@@ -13,5 +13,15 @@ namespace Ecommerce.Domain
         public decimal Price { get; set; }
         
         public string? Description { get; set; }
+
+        public int Rating { get; set; }
+
+        public string? Seller { get; set; }
+    
+        public int Stock { get; set; }
+
+        public ProductStatus Status  { get; set; }
+
+        public int CategoryId { get; set; }
     }
 }
---
+++ backend/src/Core/Ecommerce.Domain/Review.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce.Domain.Common;
+
+namespace Ecommerce.Domain
+{
+    public class Review : BaseDomainModel
+    {           
+            [Column(TypeName = "NVARCHAR(100)")]
+            public string? Name { get; set; }
+            
+            public int Rating { get; set; }
+
+            [Column(TypeName = "NVARCHAR(4000)")]
+            public string? Comment { get; set; }
+
+            public int ProductId { get; set; }    
+
+    }
+}
