e743a70d4b0020aedcaac7bc4c4a262127a5aba3
MizaelJR17 <mizael.jxjunior@aluno.educacao.pe.gov.br]>
2024-02-06 20:30:24 -0300

    OrderStatus

--- backend/src/Core/Ecommerce.Domain/Order.cs
+++ backend/src/Core/Ecommerce.Domain/Order.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Ecommerce.Domain.Common;
 
@@ -9,6 +10,26 @@ namespace Ecommerce.Domain
 {
     public class Order : BaseDomainModel
     {
+        public Order() {}
+
+        public Order(
+            string compradorNombre,
+            string compradorEmail,
+            OrderAddress orderAddress,
+            decimal subtotal,
+            decimal total,
+            decimal impuesto,
+            decimal precioEnvio
+        ){
+            CompradorNombre = compradorNombre;
+            CompradorUserName = compradorEmail;
+            OrderAddress = orderAddress;
+            Subtotal = subtotal;
+            Total = total;
+            Impuesto = impuesto;
+            PrecioEnvio = precioEnvio;
+        }
+
         public string? CompradorNombre { get; set; }
     
         public string? CompradorUserName { get; set; }
@@ -30,7 +51,12 @@ namespace Ecommerce.Domain
 
         [Column(TypeName ="decimal(10,2)")]
         public decimal PrecioEnvio { get; set; }
-
+        
+        public string? PaymentIntentId { get; set; }
+    
+        public string? ClientSecret { get; set; }
+    
+        public string? StripeApiKey { get; set; }
     }
 
 }
---
+++ backend/src/Core/Ecommerce.Domain/OrderStatus.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain
+{
+    public enum OrderStatus
+    {
+        [EnumMember(Value = "Pendente")]
+        Pending,
+
+        
+    }
+}
